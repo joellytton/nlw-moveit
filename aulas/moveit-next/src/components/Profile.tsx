1acf9bafc7350a71319301a3125645c45f89ab4e
Joellytton mendonça dos santos <joellytton25@gmail.com>
2021-02-26 15:25:33 -0500

    aula 04

--- aulas/moveit-next/src/components/Profile.tsx
+++ aulas/moveit-next/src/components/Profile.tsx
@@ -1,5 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 export function Profile() {
+    const {level} = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/joellytton.png" alt="Joellytton Mendonça"/>
@@ -7,7 +12,7 @@ export function Profile() {
                 <strong>Diego Fernandes</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                     </p>
             </div>
         </div>
